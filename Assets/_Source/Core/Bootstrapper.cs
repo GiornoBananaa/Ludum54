4b85273f6be2bb513647d3bd87ded97a202053ce
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-09-30 00:58:22 +0300

    Created Bootstrapper

---
+++ Assets/_Source/Core/Bootstrapper.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Core
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+        
+        }
+    }
+}
