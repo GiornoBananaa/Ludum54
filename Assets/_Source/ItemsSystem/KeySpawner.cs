e23c373e68b5bb02a1704db10084670553dd216f
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-10-01 17:42:32 +0300

    updated core and key prefabs,  Finished key spawner

--- Assets/_Source/ItemsSystem/KeySpawner.cs
+++ Assets/_Source/ItemsSystem/KeySpawner.cs
@@ -8,14 +8,23 @@ namespace ItemsSystem
     public class KeySpawner : MonoBehaviour
     {
         [SerializeField] private GameObject keyPrefab;
+        [SerializeField] private Transform[] firstKeySpawn;
         [SerializeField] private int count;
         [SerializeField] private int maxKeysOnOnePart;
-        
+
+        private void Awake()
+        {
+            Transform transform = firstKeySpawn[Random.Range(0, firstKeySpawn.Length)];
+            Instantiate(keyPrefab, transform.position, Quaternion.identity);
+            count--;
+        }
+
         public void Spawn(PathNode[] InfectedNodes)
         {
             int keys = Random.Range(0, maxKeysOnOnePart+1);
             for (int i = 0; i < InfectedNodes.Length; i++)
             {
+                if(count <= 0) return;
                 Transform transform = InfectedNodes[Random.Range(0, InfectedNodes.Length)].transform;
                 Instantiate(keyPrefab, transform.position, Quaternion.identity);
                 count--;
